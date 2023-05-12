f105c4f5151a62c1d63a9344418f24862ac089f0
Nalinda Gamaarachchi <44144772+NalindaGamaarachchi@users.noreply.github.com>
2023-05-12 15:42:11 +0530

    add web

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,29 @@
+export const navLinks = [
+    {
+        label: 'About',
+        pathname: '/',
+        hash: '#details',
+    },
+
+    // {
+    //     label: 'Projects',
+    //     pathname: '/',
+    //     hash: '#project-1',
+    // },
+    // {
+    //   label: 'Articles',
+    //   pathname: '/articles',
+    // },
+    {
+        label: 'Contact',
+        pathname: '/contact',
+    },
+]
+
+export const socialLinks = [
+    {
+        label: 'Github',
+        url: 'https://github.com/NalindaGamaarachchi',
+        icon: 'github',
+    },
+]
